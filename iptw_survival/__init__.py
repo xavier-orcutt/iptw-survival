3814141b532332dd0237cfb420b7810dd9974169
Xavier Orcutt <84699274+xavier-orcutt@users.noreply.github.com>
2025-11-29 08:41:10 -0500

    bumped up version

--- iptw_survival/__init__.py
+++ iptw_survival/__init__.py
@@ -4,7 +4,7 @@ IPTWSurvivalEstimator
 A Python tool for IPTW-based survival analysis.
 """
 
-__version__ = '0.1.8'
+__version__ = '0.1.9'
 
 # Make key classes available at package level
 from .iptw_survival_estimator import IPTWSurvivalEstimator
